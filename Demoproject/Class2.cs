59f656bec360d7680db14eb9a0bb76450f3fa468
rahul <rahulravi2502@gmail.com>
2022-01-14 11:53:38 +0530

    fibonacciNumber

---
+++ Demoproject/Class2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demoproject
+{
+     class ReverseNumber
+    {
+        public statc void FindReverseNumber()
+        {
+            Console.WriteLine("please enter number to reverse");
+            int number=Convert.ToInt32(Console.ReadLine()); 
+            int remainder,reverse=0;
+            while(number>0)
+            {
+                reminder = number % 10;
+                reverse = (reverse * 10) + remainder;
+                number = number / 10;
+            }
+        }
+    }
+}
